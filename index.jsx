14bf1422ded16ddbe0d21858f5350a62eeaff22c
allysonmjahn-dot <allysonmjahn@gmail.com>
2025-08-26 10:17:35 -0500

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const data = [1,2,3];
+useEffect(() => {
+  fetch("", {})
+  .then(res => res.json())
+}, [])
 
 return <div>
   <Col xs={12} md={6 lg={4} xl={3} key={data.index}}>
